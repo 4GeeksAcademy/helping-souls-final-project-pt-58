90660f5bbd3f48a967c5c4f1fe642fa96a8d5f3e
Bryan Navarro <diazbnavarro@gmail.com>
2025-12-13 18:41:26 +0000

    adding models and telationship

--- src/api/models.py
+++ src/api/models.py
@@ -5,15 +5,32 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    userID: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
     def serialize(self):
         return {
-            "id": self.id,
+            "userID": self.id,
             "email": self.email,
+            "name": self.name
             # do not serialize the password, its a security breach
+        }
+    
+class Organizer(db.Model):
+    organizadorID: Mapped[int] = mapped_column(primary_key=True)
+    userID: Mapped[str] = mapped_column(String(120), nullable=False)
+    events: Mapped[str] = mapped_column(String(120), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    org_link: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            'organizadorID': self.organizadorID,
+            'userID': self.userID,
+            'events': self.events,
+            'name': self.name,
+            'org_link': self.org_link
         }
