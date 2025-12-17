d7e637ddef36ac83e98afb2ce9b63030c04bca0f
Varinia <v.mesias.r@gmail.com>
2025-12-17 02:48:20 +0000

    cree componente del nuevo evento

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Event
+from api.models import db, User, Events, Organizer
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from datetime import datetime
@@ -30,21 +30,38 @@ def new_event():
     user = get_jwt_identity()
     data = request.get_json()
 
-    event = Event(
-        organizerID=user['id'],
+    if not data:
+        return jsonify({"msg": "No data provided"}), 400
+
+    required_fields = ['name', 'event_date', 'location', 'category', 'max_volunteers', 'description']
+    for field in required_fields:
+        if field not in data:
+            return jsonify({"msg": f"Missing field: {field}"}), 400
+
+    organizer = Organizer.query.filter_by(userID=user['id']).first()
+    if not organizer:
+        return jsonify({"msg": "User is not an organizer"}), 403
+
+    try:
+        event_date = datetime.fromisoformat(data['event_date']).date()
+    except ValueError:
+        return jsonify({"msg": "Invalid date format"}), 400
+
+    event = Events(
+        organizerID=organizer.organizerID,
         name=data['name'],
-        date=datetime.fromisoformat(data['date']),
-        address=data['address'],
+        event_date=event_date,
+        location=data['location'],
         category=data['category'],
         max_volunteers=data['max_volunteers'],
         description=data['description'],
-        review=data.get('review', True)
+        review=data.get('review')
     )
 
     db.session.add(event)
     db.session.commit()
 
     return jsonify({
-        "message": "Created event",
-        "event_id": event.id
+        "message": "Event created",
+        "event_id": event.eventID
     }), 201
