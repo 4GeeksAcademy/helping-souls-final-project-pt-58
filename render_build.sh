123fac00599cc003330c8baafeb636d4cb6f2b46
Bryan Navarro <diazbnavarro@gmail.com>
2026-04-14 20:35:14 -0400

    Upgrade pip and install pipenv in render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
+
+python -m pip install --upgrade pip
+python -m pip install pipenv
 
 npm install
 npm run build
 
 pipenv install
-
 pipenv run upgrade
