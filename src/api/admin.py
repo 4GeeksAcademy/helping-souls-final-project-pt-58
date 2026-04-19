bb14daa62994b8b2266646b13a41c9abde71ee3a
Bryan Navarro <diazbnavarro@gmail.com>
2026-04-19 13:44:00 -0400

    Update admin.py

--- src/api/admin.py
+++ src/api/admin.py
@@ -4,12 +4,11 @@ from flask_admin import Admin
 from . import models
 from .models import db
 from flask_admin.contrib.sqla import ModelView
-from flask_admin.theme import Bootstrap4Theme
 
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    admin = Admin(app, name='4Geeks Admin',
+    admin = Admin(app),
                   theme=Bootstrap4Theme(swatch='cerulean'))
 
     # Dynamically add all models to the admin interface
